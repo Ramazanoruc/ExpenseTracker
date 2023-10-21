b7b438ef4b40a08c30b1dc6de3b2513d653fca4a
Ramazan <r.orucc02@gmail.com>
2023-10-21 17:00:24 +0300

    Expense-Tracker

---
+++ Expense Tracker/ExpenseTracker/ExpenseTracker/Controllers/TransactionController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+using System.Transactions;
+
+namespace ExpenseTracker.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transactions.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+
+        // GET: Transaction/AddorEdit
+        public IActionResult AddorEdit(int id=0)
+        {
+            PopulateCategories();
+            if (id == 0)
+                return View(new transaction());
+            else
+                return View(_context.Transactions.Find(id));
+          
+        }
+
+
+
+
+		// POST: Transaction/AddorEdit
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddorEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] transaction _transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if(_transaction.TransactionId==0)
+                {
+                    _context.Add(_transaction);
+                }
+                else
+                {
+                    _context.Update(_transaction);
+                }
+
+                
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            PopulateCategories();
+            return View(_transaction);
+        }
+
+
+
+   
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool transactionExists(int id)
+        {
+          return (_context.Transactions?.Any(e => e.TransactionId == id)).GetValueOrDefault();
+        }
+
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaulCategory = new Category()
+            {
+                CategoryId = 0,
+                Title = "Choose a Category"
+            };
+            CategoryCollection.Insert(0, DefaulCategory);
+            ViewBag.Categories = CategoryCollection;
+       
+        }
+    }
+}
---
+++ Expense Tracker/ExpenseTracker/ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext:DbContext //Main Db classından inheritledim
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {}
+
+        public DbSet<transaction>Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
